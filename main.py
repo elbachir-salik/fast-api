f527b7ce5b6839139690f482e348ac5dca57af50
elbachir-salik <salik.elbachir@gmail.com>
2024-10-28 20:13:31 +0100

    added a db with 2 Users

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from models import User, Gender, Role
+from typing import List
+from uuid import uuid4
 
 app = FastAPI()
+
+db: List[User] = [
+    
+    User(id=uuid4(), fist_name="elbachir", last_name="salik", gender=Gender.male, roles=[Role.student]),
+    User(id=uuid4(), fist_name="queen", last_name="morgan", gender=Gender.female, roles=[Role.admin,Role.user])
+]
+
 
 @app.get("/")
 def read_root():
