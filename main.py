e05c7fb209ccee2ad2b83ecd4fcb3494987e3fb1
elbachir-salik <salik.elbachir@gmail.com>
2024-11-17 17:30:15 +0100

    seperate dtos from the main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel
-from typing import List, Annotated
+from dtos import QuestionBase, ChoiceBase
+from typing import  Annotated
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -10,13 +10,7 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
-class ChoiceBase(BaseModel):
-    choice_text: str
-    is_correct: bool
 
-class QuestionBase(BaseModel):
-    question_text: str
-    choices: List[ChoiceBase]
 
 def get_db():
     db = SessionLocal()
